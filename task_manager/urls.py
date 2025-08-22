34e80a81c6a7099d75cdf14dcbdd8bb6728f9094
Vasyl Boliukh <vskesha@gmail.com>
2025-08-22 13:56:25 +0300

    implemented update, detail and delete for tasks

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,44 @@
 from django.urls import path
 
-from task_manager.views import TaskListView, TaskCreateView
+from task_manager.views import (
+    TaskCreateView,
+    TaskDeleteView,
+    TaskDetailView,
+    TaskListView,
+    TaskUpdateView,
+)
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list"),
-    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path(
+        "",
+        TaskListView.as_view(),
+        name="task-list"
+    ),
+    path(
+        "tasks/",
+        TaskListView.as_view(),
+        name="task-list"
+    ),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "tasks/<int:pk>/detail/",
+        TaskDetailView.as_view(),
+        name="task-detail"
+    ),
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    )
 ]
 
 app_name = "task_manager"
